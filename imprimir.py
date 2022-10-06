7e112b14e9593f9ca3a78ecb4954ca9db4faa1c2
CadPaciente <111750769+CadPaciente@users.noreply.github.com>
2022-10-06 09:37:49 -0300

    MÉTODO PARA IMPRESSÃO DE ARQUIVOS

--- imprimir.py
+++ imprimir.py
@@ -1,11 +1,10 @@
-import win32print
-import win32api
+#import win32print
+#import win32api
+
 import os
+
 os.startfile(r'C:\Users\Junior\Desktop\ATESTADO.pdf')
 
 #impressoras = win32print.EnumPrinters(2)
-
-
 #impressora = impressoras[3]
-
 #win32print.SetDefaultPrinter()
