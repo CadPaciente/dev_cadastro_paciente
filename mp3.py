8b8786c8ffa1b652cc5bf463da92876365d99611
Manoel Júnior <python.iniciante@gmail.com>
2022-10-06 06:56:10 -0300

    CADASTRO DE PACIENTES

---
+++ mp3.py
@@ -0,0 +1,32 @@
+import pygame
+
+def tears():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\Tears for Fears.mp3')
+    pygame.mixer.music.play(loops=0)
+
+def depeche():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\Depeche Mode.mp3')
+    pygame.mixer.music.play(loops=0)
+
+def weekend():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\The Weeknd.mp3')
+    pygame.mixer.music.play(loops=0)
+
+def eddie():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\Eddie Vedder.mp3')
+    pygame.mixer.music.play(loops=0)
+
+def coldplay():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\Coldplay.mp3')
+    pygame.mixer.music.play(loops=0)
+
+def newOrder():
+    pygame.mixer.init()
+    pygame.mixer.music.load(r'C:\dev_cadastro_paciente\MP3\New Order.mp3')
+    pygame.mixer.music.play(loops=0)
+
