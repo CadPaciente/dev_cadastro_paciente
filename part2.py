28f34f3799ee0aca5f6b94493ed1d124bd315b08
CadPaciente <111750769+CadPaciente@users.noreply.github.com>
2022-10-06 09:49:07 -0300

    WIDGETS - PARTE 2

--- part2.py
+++ part2.py
@@ -17,24 +17,25 @@ class principa():
         self.widgets_frame1()
 
         root.mainloop()
-
+        
+# Tela principal
     def tela(self):
         self.root.configure(bg='#008888')
         self.root.geometry("1366x768+0+0")
         self.root.overrideredirect(True)
 
     def cor_widgets(self):
-        # Botões
+# Botões
         self.bt_bg = '#008888'
         self.bt_fg = 'white'
         self.bt_font = ('verdana', 13, 'bold')
 
-        # Label
+# Label
         self.lb_bg = '#008888'
         self.lb_fg = 'white'
         self.lb_font = ('arial', 15, 'bold')
 
-        # Entrada de dados
+# Entrada de dados
         self.et_bg = '#008888'
         self.et_bg_branco = 'white'
         self.et_fg_branco = "#ffffff"
